99192ba18ac64f9e68620d0a2bec8187c2957fa2
Sharry-Li <lixuyanzju@gmail.com>
2020-03-20 11:05:37 -0700

    deploy

---
+++ docs/precache-manifest.faa3f96366da9ea2313214083c1f35fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7292ac1b2beecc3e0fdc1ef6e85ea86e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba1c60e775f57be14a69",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "f52753556875d304f78a",
+    "url": "/static/css/main.784246bb.chunk.css"
+  },
+  {
+    "revision": "ba1c60e775f57be14a69",
+    "url": "/static/js/2.7a473bd4.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.7a473bd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f52753556875d304f78a",
+    "url": "/static/js/main.b060846b.chunk.js"
+  },
+  {
+    "revision": "63f63d69bf3274195ee2",
+    "url": "/static/js/runtime-main.ca9e6c5c.js"
+  }
+]);
