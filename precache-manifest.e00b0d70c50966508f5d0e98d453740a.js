a2612b5e2e2aa498fb50d6a337d357b7903114b2
Sharry-Li <lixuyanzju@gmail.com>
2020-03-20 11:37:49 -0700

    gh-pages

---
+++ precache-manifest.e00b0d70c50966508f5d0e98d453740a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62263ae544c5a809e8017e2a8bf415f1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ba1c60e775f57be14a69",
+    "url": "./static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ffabb410c58e12f82ff7",
+    "url": "./static/css/main.784246bb.chunk.css"
+  },
+  {
+    "revision": "ba1c60e775f57be14a69",
+    "url": "./static/js/2.7a473bd4.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "./static/js/2.7a473bd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffabb410c58e12f82ff7",
+    "url": "./static/js/main.0d15a689.chunk.js"
+  },
+  {
+    "revision": "510bd5ee9d4372510d8c",
+    "url": "./static/js/runtime-main.63a8fbab.js"
+  }
+]);
